9fc1f1c93e6da9dfb644dd487374f6af2e2a54ab
Brian Glowe <brianglowe@gmail.com>
2016-02-09 22:00:45 -0500

    Added Player class which inherits from the Character class: 1. Used convenience initializers & computed properties for private variables

---
+++ rpgoop/Player.swift
@@ -0,0 +1,34 @@
+//
+//  Player.swift
+//  rpgoop
+//
+//  Created by Brian J Glowe on 2/9/16.
+//  Copyright © 2016 BG. All rights reserved.
+//
+
+import Foundation
+
+class Player: Character {
+    
+    private var _name: String = "Player"
+    
+    var name: String {
+        get {
+            return _name
+        }
+    }
+    
+    private var _inventory = [String]()
+    
+    var inventory: [String] {
+        get {
+            return _inventory
+        }
+    }
+    
+    convenience init(name: String, hp: Int, attackPwr: Int) {
+        self.init(startingHp: hp, attackPwr: attackPwr)
+
+        _name = name
+    }
+}
